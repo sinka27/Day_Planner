45e84138de89bd6326c43c13ccefba3975fe158a
sinka27 <sinka27392@gmail.com>
2022-01-07 16:12:13 -0800

    1.Added event listener to save button. 2. displaying the previously saved user data upon reload

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ var currentHour = moment().format("HH");
 var today = moment();
 $("#currentDay").text(today.format("dddd , MMMM Do YYYY "));
 
+//Add event listener for save button
+$(".saveBtn").each(function (index) {
+  $(this).on("click", function () {
+    var plan = inputGroupEl.eq(index).val();
+    //saving user data to local storage
+    localStorage.setItem(index, plan);
+  });
+});
+//upon reload displaying the previously saved user data
+for (var i = 0; i < inputGroupEl.length; i++) {
+  inputGroupEl.eq(i).val(localStorage.getItem(i));
+}
+
+
