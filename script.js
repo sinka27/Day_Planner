2e80bd17fdae4f724ce9d8adb4e73e12d0aad5cd
sinka27 <sinka27392@gmail.com>
2022-01-07 16:11:00 -0800

    Displaying current Day and date at top of calendar

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-//Displaying current date at top of calendar
+//Global variables
+var inputGroupEl = $(".form-control");
+var hourEl = $(".hour");
+var currentHour = moment().format("HH");
+
+//Displaying current day and the date at the top of calendar
 var today = moment();
-$("#currentDay").text(today.format("dddd , MMMM Do "));
+$("#currentDay").text(today.format("dddd , MMMM Do YYYY "));
+
